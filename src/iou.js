81ce7ad9677ac4ee26cf686b5a17c52b1d96a672
Yuri Gorokhov <yurigorokhov@gmail.com>
2014-04-18 22:31:44 -0700

    adding error service

--- src/iou.js
+++ src/iou.js
@@ -76,14 +76,45 @@ iouApp.service('UserService', function(user) {
     this.User = null;
   };
 });
+iouApp.service('ErrorService', function() {
+  this._errorId = 0;
+  this.Errors = [];
+  this.errorMap = {};
+  this.clean = function() {
+    this.Errors = [];
+    this.errorMap = {};
+  };
+  this.showError = function(msg) {
+    var self = this;
+    var id = self._errorId++;
+    self.errorMap[id] = msg;
+    self.Errors.push(msg);
+    return {
+      remove: function() {
+        self.errorMap[id] = null;
+        self.Errors = _(self.errorMap)
+          .chain()
+          .filter(function(x) { 
+            return x !== null; 
+          })
+          .values()
+          .value();
+      }
+    };
+  };
+});
 
 //--- Controllers ---
 var controllers = angular.module('controllers', ['ui.bootstrap']);
-controllers.controller('ParentCtrl', ['$scope', '$location', 'user', 'UserService',
-  function ($scope, $location, user, UserService) {
+controllers.controller('ParentCtrl', ['$scope', '$location', 'user', 'UserService', 'ErrorService',
+  function ($scope, $location, user, UserService, ErrorService) {
     $scope.UserService = UserService;
+    $scope.ErrorService = ErrorService;
     $scope.$watch('UserService.User', function (newValue) {
         $scope.currentUser = newValue;
+    });
+    $scope.$watch('ErrorService.Errors', function (newValue) {
+        $scope.errors = newValue;
     });
     user.getCurrent().then(function(currentUser) {
       UserService.setUser(currentUser);
@@ -99,11 +130,11 @@ controllers.controller('DashboardCtrl', ['$scope',
   	$scope.tasks = dummyTasks;
   }
 ]);
-controllers.controller('LoginCtrl', ['$scope', '$location', 'usSpinnerService', 'user', 'blockUI', 'UserService',
-  function($scope, $location, usSpinnerService, $user, $blockUI, UserService) {
+controllers.controller('LoginCtrl', ['$scope', '$location', 'usSpinnerService', 'user', 'blockUI', 'UserService', 'ErrorService',
+  function($scope, $location, usSpinnerService, $user, $blockUI, UserService, ErrorService) {
     $scope.login = function() {
       $blockUI.start();
-      $scope.loginError = false;
+      ErrorService.clean();
       $user.facebookLogin().then(
         function(user) {
           UserService.setUser(user);
@@ -111,7 +142,7 @@ controllers.controller('LoginCtrl', ['$scope', '$location', 'usSpinnerService', 
           $blockUI.stop();
         }, function(error) {
           $blockUI.stop();
-          $scope.loginError = true;
+          ErrorService.showError('There was an error logging in, please try again.');
           UserService.logOut();
         });
       };
@@ -141,9 +172,17 @@ controllers.controller('NewTaskModal', ['$scope', '$modalInstance', 'tasks', 'it
   	};
   }
 ]);
-controllers.controller('ExploreCtrl', ['$scope', '$modal', '$q', 'rewards', 'tasks', 'blockUI',
-  function ($scope, $modal, $q, rewards, tasks, $blockUI) {
+controllers.controller('ExploreCtrl', ['$scope', '$modal', '$q', 'rewards', 'tasks', 'blockUI', 'ErrorService',
+  function ($scope, $modal, $q, rewards, tasks, $blockUI, ErrorService) {
     var rewardsResultById = {};
+    $scope.deleteTask = function(task) {
+      ErrorService.clean();
+      tasks.deleteTask(task).then(function() {
+        $scope.refreshTasks();
+      }, function() {
+        ErrorService.showError('There was an error deleting the task');
+      });
+    };
     $scope.refreshTasks = function() {
       $blockUI.start();
       $q.all([tasks.get(), rewards.getById()]).then(
