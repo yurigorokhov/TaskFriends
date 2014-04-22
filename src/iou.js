743f128e8fb963c7df21dcca44845d08cbc0f626
Yuri Gorokhov <yurigorokhov@gmail.com>
2014-04-22 00:08:47 -0700

    Implemented dissapearing alerts

--- src/iou.js
+++ src/iou.js
@@ -76,47 +76,70 @@ iouApp.service('UserService', function(user) {
     this.User = null;
   };
 });
-iouApp.service('ErrorService', function() {
-  this._errorId = 0;
-  this.Errors = [];
-  this.errorMap = {};
-  this.clean = function() {
-    this.Errors = [];
-    this.errorMap = {};
-  };
-  this.showError = function(msg) {
+iouApp.service('AlertService', ['$timeout', function($timeout) {
+  this._observers = [];
+  this._alertId = 0;
+  this._alerts = [];
+  this._modifyAlerts = function(func) {
     var self = this;
-    var id = self._errorId++;
-    self.errorMap[id] = msg;
-    self.Errors.push(msg);
+    self._alerts = func(self._alerts);
+    _(self._observers).each(function(func) { func(self._alerts); });
+  };
+  this.addObserver = function(observer) {
+    this._observers.push(observer);
+  };
+  this.clean = function() {
+    this._modifyAlerts(function() { return []; });
+  };
+  this.error = function(msg) {
+    return this._message(msg, 'error');
+  };
+  this.success = function(msg) {
+    return this._message(msg, 'success');
+  };
+  this._message = function(msg, type) {
+    var self = this;
+    type = type || 'success';
+    var id = self._alertId++;
+    self._modifyAlerts(function(alerts) { 
+      alerts.push({ id: id, text: msg, type: type });
+      return alerts;
+    });
+    var removeSelf = function() {
+        self._modifyAlerts(function(alerts) {
+          return alerts.filter(function(x) { 
+            return x.id !== id; 
+          });
+        });
+      };
     return {
-      remove: function() {
-        self.errorMap[id] = null;
-        self.Errors = _(self.errorMap)
-          .chain()
-          .filter(function(x) { 
-            return x !== null; 
-          })
-          .values()
-          .value();
+      remove: removeSelf,
+      timeout: function(milliseconds) {
+        milliseconds = milliseconds || 3000;
+        $timeout(removeSelf, milliseconds);
       }
     };
   };
-});
+}]);
 
 //--- Controllers ---
 var controllers = angular.module('controllers', ['ui.bootstrap']);
-controllers.controller('ParentCtrl', ['$scope', '$location', 'user', 'UserService', 'ErrorService',
-  function ($scope, $location, user, UserService, ErrorService) {
+controllers.controller('ParentCtrl', ['$scope', '$location', 'user', 'UserService', 'AlertService',
+  function ($scope, $location, user, UserService, AlertService) {
+    $scope.errors = [];
+    $scope.successes = [];
     $scope.dashboardActive = '';
     $scope.exploreActive = '';
     $scope.UserService = UserService;
-    $scope.ErrorService = ErrorService;
+    $scope.AlertService = AlertService;
     $scope.$watch('UserService.User', function (newValue) {
         $scope.currentUser = newValue;
     });
-    $scope.$watch('ErrorService.Errors', function (newValue) {
-        $scope.errors = newValue;
+    AlertService.addObserver(function (newValue) {
+      $scope.errors = _(newValue)
+        .filter(function(x) { return x.type === 'error'; });
+      $scope.successes = _(newValue)
+        .filter(function(x) { return x.type === 'success'; });
     });
     $scope.$on('$locationChangeSuccess', function(e) {
       switch($location.url()) {
@@ -139,8 +162,8 @@ controllers.controller('ParentCtrl', ['$scope', '$location', 'user', 'UserServic
     };
   }
 ]);
-controllers.controller('DashboardCtrl', ['$scope', 'tasks', 'blockUI', 'UserService', 'ErrorService', '$q',
-  function ($scope, tasks, $blockUI, UserService, ErrorService, $q) {
+controllers.controller('DashboardCtrl', ['$scope', 'tasks', 'blockUI', 'UserService', 'AlertService', '$q',
+  function ($scope, tasks, $blockUI, UserService, AlertService, $q) {
   	$scope.todoTasks = [];
     $scope.assetTasks = [];
     $scope.debtTasks = [];
@@ -166,26 +189,27 @@ controllers.controller('DashboardCtrl', ['$scope', 'tasks', 'blockUI', 'UserServ
           $scope.debtTasks = tasks.populatePermissions(debtTasks, UserService.User);
         },
         function() {
-          ErrorService.showError('There was an error loading tasks');
+          AlertService.error('There was an error loading tasks');
         });
     $scope.deleteTask = function(task) {
-      ErrorService.clean();
+      AlertService.clean();
       tasks.deleteTask(task).then(function() {
+        AlertService.success('Your task was successfully deleted').timeout();
         $scope.myOpenTasks = _($scope.myOpenTasks)
           .filter(function(t) {
             return t.id !== task.id;
           });
       }, function() {
-        ErrorService.showError('There was an error deleting the task');
+        AlertService.error('There was an error deleting the task');
       });
     };
   }
 ]);
-controllers.controller('LoginCtrl', ['$scope', '$location', 'usSpinnerService', 'user', 'blockUI', 'UserService', 'ErrorService',
-  function($scope, $location, usSpinnerService, $user, $blockUI, UserService, ErrorService) {
+controllers.controller('LoginCtrl', ['$scope', '$location', 'usSpinnerService', 'user', 'blockUI', 'UserService', 'AlertService',
+  function($scope, $location, usSpinnerService, $user, $blockUI, UserService, AlertService) {
     $scope.login = function() {
       $blockUI.start();
-      ErrorService.clean();
+      AlertService.clean();
       $user.facebookLogin().then(
         function(user) {
           UserService.setUser(user);
@@ -193,14 +217,14 @@ controllers.controller('LoginCtrl', ['$scope', '$location', 'usSpinnerService', 
           $blockUI.stop();
         }, function(error) {
           $blockUI.stop();
-          ErrorService.showError('There was an error logging in, please try again.');
+          AlertService.error('There was an error logging in, please try again.');
           UserService.logOut();
         });
       };
   }
 ]);
-controllers.controller('NewTaskModal', ['$scope', '$modalInstance', 'tasks', 'items',
-  function($scope, $modalInstance, tasks, items) {
+controllers.controller('NewTaskModal', ['$scope', '$modalInstance', 'tasks', 'items', 'AlertService',
+  function($scope, $modalInstance, tasks, items, AlertService) {
     $scope.task = {
       title: '',
       description: '',
@@ -210,6 +234,7 @@ controllers.controller('NewTaskModal', ['$scope', '$modalInstance', 'tasks', 'it
       tasks.add($scope.task).then(
         function() {
           $modalInstance.close();
+          AlertService.success('Your task was created successfully').timeout();
           items.refresh();
       }, function() {
       });
@@ -219,14 +244,14 @@ controllers.controller('NewTaskModal', ['$scope', '$modalInstance', 'tasks', 'it
   	};
   }
 ]);
-controllers.controller('ExploreCtrl', ['$scope', '$modal', '$q', 'tasks', 'blockUI', 'ErrorService', 'UserService',
-  function ($scope, $modal, $q, tasks, $blockUI, ErrorService, UserService) {
+controllers.controller('ExploreCtrl', ['$scope', '$modal', '$q', 'tasks', 'blockUI', 'AlertService', 'UserService',
+  function ($scope, $modal, $q, tasks, $blockUI, AlertService, UserService) {
     $scope.claim = function(task) {
-      ErrorService.clean();
+      AlertService.clean();
       tasks.claimTask(task).then(function() {
         $scope.refreshTasks();
       }, function() {
-        ErrorService.showError('There was an error claiming the task');
+        AlertService.error('There was an error claiming the task');
       });
     };
     $scope.refreshTasks = function() {
